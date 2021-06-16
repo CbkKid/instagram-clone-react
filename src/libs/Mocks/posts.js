ca45503c05ca38ad89ea329d5101c9696c114d61
TheReal7 <meir.barnovski@gmail.com>
2021-06-16 18:59:29 +0300

    added users & posts mock

---
+++ src/libs/Mocks/posts.js
@@ -0,0 +1,91 @@
+export const posts = [
+    {
+      postId: 1,
+      user: {
+        avatar:
+          "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F01%2F02%2Fcooking.jpg",
+        username: "thefoodfeed"
+      },
+      img: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/03/18/0/FNK_HOT_POT_AT_HOME_OPENER_H_f_s4x3.jpg.rend.hgtvcom.616.493.suffix/1616107469470.jpeg",
+      likesCount: 7,
+      isLikedByUser:true,
+      description:
+        "look at all of those flavors!",
+      comments: [
+        {
+          id: 1,
+          user: {
+            username: "johnyz",
+            avatar:
+              "https://yt3.ggpht.com/ytc/AAUvwniLiwFIx3rZFcIB2n8JY5D6hqGiAO7wVUiWNA=s900-c-k-c0x00ffffff-no-rj"
+          },
+          text: "so much food!"
+        },
+        {
+          id: 21,
+          user: {
+            username: "onlydrums",
+            avatar:
+              "https://www.rollingstone.com/wp-content/uploads/2019/09/shutterstock_1476813794.jpg?resize=1800,1200&w=450"
+          },
+          text: "damn!"
+        },
+        {
+          id: 3,
+          user: {
+            username: "thetraveler",
+            avatar: "https://image.freepik.com/free-photo/young-man-traveler-with-backpack-relaxing-outdoor_1421-190.jpg"
+          },
+          text: "wow that looks tasty!"
+        }
+      ]
+    },
+    {
+      postId: 2,
+      user: {
+        avatar:
+          "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F01%2F02%2Fcooking.jpg",
+        username: "thefoodfeed"
+      },
+      img:
+        "https://thewoksoflife.com/wp-content/uploads/2020/11/hot-pot-at-home-10-599x599.jpg",
+      likesCount: 7,
+      description: "another great dish!",
+      comments: [
+        {
+          id: 1,
+          user: {
+            username: "johnyz",
+            avatar:
+              "https://yt3.ggpht.com/ytc/AAUvwniLiwFIx3rZFcIB2n8JY5D6hqGiAO7wVUiWNA=s900-c-k-c0x00ffffff-no-rj"
+          },
+          text: "i would love to taste it!"
+        },
+      ]
+    },
+    {
+      postId: 3,
+      user: {
+        avatar:
+          "https://image.freepik.com/free-photo/young-man-traveler-with-backpack-relaxing-outdoor_1421-190.jpg",
+        username: "thetraveler"
+      },
+      img:
+        "https://skift.com/wp-content/uploads/2021/02/GettyImages-1059344876-1024x684.jpg",
+      likesCount: 4,
+      description: "Well... i have no words for this place",
+      comments: [
+        {
+          id: 1,
+          user: {
+            username: "johnyz",
+            avatar:
+              "https://yt3.ggpht.com/ytc/AAUvwniLiwFIx3rZFcIB2n8JY5D6hqGiAO7wVUiWNA=s900-c-k-c0x00ffffff-no-rj"
+          },
+          text: "where are you now?"
+        }
+      ]
+    }
+  ];
+
+  
---
+++ src/libs/Mocks/users.js
@@ -0,0 +1,107 @@
+export const users = [
+    {
+      username: "thefoodfeed",
+      fullname: "tasty",
+      avatar:
+        "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F01%2F02%2Fcooking.jpg",
+      bio: "there are no limits to tasty food you can make",
+      info: {
+        followers: 5,
+        following: 6
+      },
+      posts: [
+        {
+          postId: 1,
+          user: {
+            avatar:
+              "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F01%2F02%2Fcooking.jpg",
+            username: "thefoodfeed"
+          },
+          img:
+            "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/03/18/0/FNK_HOT_POT_AT_HOME_OPENER_H_f_s4x3.jpg.rend.hgtvcom.616.493.suffix/1616107469470.jpeg",
+          likesCount: 7,
+          description:
+            "Give it , Give it , Give it to Edi Cavani! pass him the ball and he will score all the goals!",
+          comments: [
+            {
+              id: 1,
+              username: "bfernandes",
+              text: "what a magnificent goal!"
+            },
+            {
+              id: 2,
+              username: "paulPogba",
+              text: "damn!"
+            },
+            {
+              id: 3,
+              username: "Edinson Cavani",
+              text: "that was fun!"
+            }
+          ]
+        },
+        {
+          postId: 2,
+          user: {
+            avatar:
+              "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F01%2F02%2Fcooking.jpg",
+            username: "wanBissaka"
+          },
+          img:
+            "https://thewoksoflife.com/wp-content/uploads/2020/11/hot-pot-at-home-10-599x599.jpg",
+          likesCount: 7,
+          description:
+            "Give it , Give it , Give it to Edi Cavani! pass him the ball and he will score all the goals!",
+          comments: [
+            {
+              id: 1,
+              username: "bfernandes",
+              text: "what a magnificent goal!"
+            },
+            {
+              id: 2,
+              username: "paulPogba",
+              text: "damn!"
+            },
+            {
+              id: 3,
+              username: "Edinson Cavani",
+              text: "that was fun!"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      username: "thetraveler",
+      fullname: "the traveler",
+      avatar:
+        "https://image.freepik.com/free-photo/young-man-traveler-with-backpack-relaxing-outdoor_1421-190.jpg",
+      bio: "Around the world!",
+      info: {
+        followers: 5,
+        following: 6
+      },
+      posts: [
+        {
+          postId: 3,
+          user: {
+            avatar:
+              "https://image.freepik.com/free-photo/young-man-traveler-with-backpack-relaxing-outdoor_1421-190.jpg",
+            username: "theTraveler"
+          },
+          img:
+            "https://skift.com/wp-content/uploads/2021/02/GettyImages-1059344876-1024x684.jpg",
+          likesCount: 4,
+          description: "Well... i have no words for this place",
+          comments: [
+            {
+              id: 1,
+              username: "johnyz",
+              text: "where are you now?"
+            }
+          ]
+        }
+      ]
+    }
+  ];
