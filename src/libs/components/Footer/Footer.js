f469cc2cc1b0e170480d975436faf63bdadf4d93
TheReal7 <meir.barnovski@gmail.com>
2021-06-30 14:45:29 +0300

    added link to github repository to footer

--- src/libs/components/Footer/Footer.js
+++ src/libs/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Footer.css";
+import GithubLogo from '../../../assets/GitHub-Mark-32px.png';
 
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Copy Rights 2020 Made By </p>
+      <p><a href="https://www.github.com/mb-cbk/instagram-clone-react" target="_blank"><img src={GithubLogo}/></a></p>
     </div>
   );
 }
